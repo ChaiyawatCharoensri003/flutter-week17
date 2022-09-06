b69186f10d07fd6e2bdda5d5799ffe45fe5cb637
ChaiyawatCharoensri003 <nra79318@gmail.com>
2022-09-06 14:20:53 +0700

    first commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'home.dart';
 class MyApp extends StatelessWidget {
-//l.create constructor
   const MyApp({Key? key}) : super(key: key);
-//2.create build
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
